db1856c74f313685fa8e2d9e847e23153c276fd4
drewproud <drew.proud@arcadiasolutions.com>
2014-08-25 01:40:54 -0400

    Added some logic to the monitorList list

--- MonitorManager/MonitorManager/MonitorManager/Form1.cs
+++ MonitorManager/MonitorManager/MonitorManager/Form1.cs
@@ -12,24 +12,75 @@ namespace MonitorManager
 {
     public partial class Form1 : Form
     {
+
+        // It hangs after I select the second monitor
+
+        private Dictionary<string, string> monitorList = new Dictionary<string, string>
+        {
+            {"leftMonitor", ""},
+            {"mainMonitor",""},
+            {"rightMonitor", ""}
+        };
+
         public Form1()
         {
             InitializeComponent();
 
-            string leftMonitor = null;
-            string mainMonitor;
-            string rightMonitor = null;
+            InitializeCombo(comboBox1);
+            InitializeCombo(comboBox2);
+
+        }
+
+        public List<string> GetMonitorList()
+        {
+            List<string> thisList = (List<string>)Screen.AllScreens.Select(hello => hello.DeviceName).ToList();
+            thisList.Add("<None>");
+            return thisList;
+        }
+
+        public Dictionary<string, string> MonitorList
+        {
+            get { return monitorList; }
         }
 
         private void InitializeCombo(ComboBox combo)
         {
-            List<string> screens = (List<string>)Screen.AllScreens.Select(hello => hello.DeviceName).ToList();
+            // Creates the list of all screens attached to the computer
+            List<string> screens = GetMonitorList();
 
-            // Add the screens available to the combobox
+            string currentText = combo.Text;
+
+            // Clear all existing items in the current combobox
+            combo.Items.Clear();
+
+            Dictionary<string, string> mList = this.monitorList;
+
+            // Add the screens available to the combobox that are not used in any other boxes
             foreach (string mName in screens)
             {
-                comboBox1.Items.Add(mName);
+                if (!mList.Values.Any(mlist => mlist.Equals(mName)))
+                {
+                    combo.Items.Add(mName);
+                }
+            }
 
+            // Reset it to the original selection
+            combo.Text = currentText;
+        }
+
+        private void InitializeOtherCombos(ComboBox cBox)
+        {
+            Dictionary<string, string> mDict = this.monitorList; 
+
+            foreach (Control combo in this.Controls)
+            {
+                if (combo is ComboBox)
+                {
+                    if (cBox.Name != combo.Name)
+                    {
+                        InitializeCombo(combo as ComboBox);
+                    }
+                }
             }
         }
 
@@ -77,12 +128,12 @@ namespace MonitorManager
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            InitializeOtherCombos(comboBox1);
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            InitializeOtherCombos(comboBox2);
         }
     }
 }
